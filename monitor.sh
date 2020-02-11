ba9ebc0e7e9eb017982fe17fd6504af17ab98cf4
f0m41h4u7 <fomalhaut_a3v@outlook.com>
2020-02-11 23:41:30 +0300

    first version

---
+++ monitor.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Get number of workers that should be running (assuming the number is being passed as the parameter to the script)
+qty=$1
+
+# Check number of connections to rabbitmq
+connections=$(rabbitmqctl list_connections | tail -n +1)
+working=$(connections | wc -l)
+
+# Compare with overally number of workers
+# If some workers are not connected, send notification
+if [[ qty>working ]]; then
+	curl -X POST https://notify.bot.ifmo.su/u/ABCD1234 -d "Some workers have died! A necromancer is needed!"
+fi
