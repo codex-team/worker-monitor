d659ce3efdbea550c7adbf8756a047a31165930d
f0m41h4u7 <fomalhaut_a3v@outlook.com>
2020-02-12 18:14:17 +0300

    fixed issue with variables

--- monitor.sh
+++ monitor.sh
@@ -5,10 +5,10 @@ qty=$1
 
 # Check number of connections to rabbitmq
 connections=$(rabbitmqctl list_connections | tail -n +1)
-working=$(connections | wc -l)
+working=$(echo $connections | wc -l)
 
 # Compare with overally number of workers
 # If some workers are not connected, send notification
-if [[ qty>working ]]; then
+if [[ $qty>$working ]]; then
 	curl -X POST https://notify.bot.ifmo.su/u/ABCD1234 -d "Some workers have died! A necromancer is needed!"
 fi
