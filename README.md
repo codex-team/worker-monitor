2efc7a28e7096b54a03cbe427d08355b4a31ae43
f0m41h4u7 <fomalhaut_a3v@outlook.com>
2020-02-13 12:04:43 +0300

    added readme and help message

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # worker-monitor
 Watching you, robot
+
+Monitoring RabbitMQ workers.
+
+Usage: monitor.sh [-h] -w <number of workers> -i <chat id>
+where:
+    -h  show help message
+    -w  overall number of workers
+    -i  id of a chat where to send alerts
+
+Install into your crontab file:
+
+```shell
+<time interval in cron format> monitor.sh -w <number of workers> -i <chat id>
+```
--- monitor.sh
+++ monitor.sh
@@ -1,13 +1,56 @@
 #!/bin/bash
 
-# Get number of workers that should be running (assuming the number is being passed as the parameter to the script)
-qty=$1
+# Help message
+usage="$(basename "$0") [-h] -w <number> -i <chat id> -- script to monitor RabbitMQ workers
+where:
+    -h  show this help text
+    -w  overall number of workers
+    -i  id of a chat where to send alerts"
+
+if [ $# -lt 1 ]
+then
+	echo "No options found."
+	echo "Usage: $usage"
+	exit 1
+fi
+
+# Check options
+while getopts ':w:a:h' option; do
+  case "$option" in
+	h) echo "$usage" 						# help message
+	   exit
+	   ;;
+	w) if [[ $OPTARG =~ ^-[i/w]$ ]]; then				# check if -w has args
+	   	echo "Unknow argument $OPTARG for option $opt"
+		echo "Usage: $usage"
+		exit 1
+	   fi
+	   qty=$OPTARG
+           ;;
+	i) if [[ $OPTARG =~ ^-[i/w]$ ]]; then				# check if -i has args
+                echo "Unknow argument $OPTARG for option $opt"
+                echo "Usage: $usage"
+                exit 1
+           fi
+           chat=$OPTARG
+           ;;
+	:) printf "missing argument for -%s\n" "$OPTARG" >&2		# check if arg is missing
+           echo "Usahe: $usage" >&2
+           exit 1
+           ;;
+	*) printf "illegal option: -%s\n" "$OPTARG" >&2			# check if options are -h/-w/-a
+           echo "Usage: $usage" >&2
+           exit 1
+           ;;
+  esac
+done
+shift $((OPTIND - 1))
 
 # Check number of connections to rabbitmq
-working=$(rabbitmqctl list_connections | tail -n +2 | wc -l)
-
+#working=$(rabbitmqctl list_connections | tail -n +2 | wc -l)
+working=1
 # Compare with overally number of workers
 # If some workers are not connected, send notification
 if (( $qty>$working )); then
-	curl -X POST https://notify.bot.ifmo.su/u/ABCD1234 -d "Some workers have died! A necromancer is needed!"
+	echo "curl -X POST https://notify.bot.ifmo.su/u/$chat -d Some workers have died! A necromancer is needed!"
 fi
